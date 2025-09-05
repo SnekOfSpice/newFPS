e98572b7feb16c1609c9b34693be60a9b22c945d
Fio <leukert.fio@gmail.com>
2025-09-05 23:09:10 +0200

    so much dithering

--- player.gd
+++ player.gd
@@ -5,29 +5,29 @@ class_name Player
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var speed := 2.0
-var jump_speed = 3
 var step_bob := 0.04
 var step_fov_change := 5.0
 var mouse_sensitivity = 0.002
-var input_direction := Vector3.ZERO # raw
-var direction := Vector3.ZERO # lerped
 
-var is_down_pressed := false
-var is_up_pressed := false
-var is_left_pressed := false
-var is_right_pressed := false
+@onready var camera : Camera3D = find_child("Camera3D")
 
-@onready var camera_base_position : Vector3 = $Camera3D.position
-@onready var camera_base_fov : float = $Camera3D.fov
+@onready var camera_base_position : Vector3 = camera.position
+@onready var camera_base_fov : float = camera.fov
 
 func _ready() -> void:
 	DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_CAPTURED)
 
 func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		if DisplayServer.mouse_get_mode() == DisplayServer.MOUSE_MODE_CAPTURED:
+			DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_VISIBLE)
+		else:
+			DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_CAPTURED)
+	
 	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		rotate_y(-event.relative.x * mouse_sensitivity)
-		$Camera3D.rotate_x(-event.relative.y * mouse_sensitivity)
-		$Camera3D.rotation.x = clampf($Camera3D.rotation.x, -deg_to_rad(70), deg_to_rad(70))
+		camera.rotate_x(-event.relative.y * mouse_sensitivity)
+		camera.rotation.x = clampf(camera.rotation.x, -deg_to_rad(70), deg_to_rad(70))
 	
 	if event.is_action("ui_up"):
 		queue_step()
@@ -47,7 +47,7 @@ func queue_step():
 	else:
 		t.tween_property(self, "step_strength", 0.4, 0.3).set_ease(Tween.EASE_OUT_IN).set_trans(Tween.TRANS_SINE)
 		t.tween_property(self, "step_strength", 0.2, 0.2).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
-		t.tween_property(self, "step_strength", 0, 0.1).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_EXPO)
+		t.tween_property(self, "step_strength", 0, 0.1).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
 		
 	
 	#t.finished.connect(on_end_step)
@@ -61,6 +61,7 @@ func queue_step():
 var input: Vector2
 
 func _physics_process(delta):
+	%TextureRect.texture = camera.get_viewport().get_texture()
 	velocity.y += -gravity * delta
 	var raw_input := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if step_strength == 0:
@@ -77,8 +78,8 @@ func _physics_process(delta):
 	#if is_on_floor() and Input.is_action_just_pressed("jump"):
 		#velocity.y = jump_speed
 	
-	$Camera3D.position = camera_base_position + Vector3.UP * step_strength * step_bob
-	$Camera3D.fov = camera_base_fov + step_strength * step_fov_change
+	camera.position = camera_base_position + Vector3.UP * step_strength * step_bob
+	camera.fov = camera_base_fov + step_strength * step_fov_change
 
 #func _unhandled_input(event: InputEvent) -> void:
 	
